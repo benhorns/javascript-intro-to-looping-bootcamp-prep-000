e1b86e2ee24750672359daccde8dd64ba55c7905
Benjamin Horns <benhorns@gmail.com>
2019-12-23 20:37:00 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,6 +1,14 @@
-function forLoop(array){
+function forLoop(array) {
  for (var i = 0; i < 25; i++) {
-    if (i===1) {console.log("I am " + i + " strange loop.")}
-    else {console.log("I am" + i + " strange loops.")}
+    if (i===1) {array.push("I am " + i + " strange loop.")}
+    else {array.push("I am " + i + " strange loops.")}
   }
+  return array
+
 }
+//
+// function whileLoop(n){
+//   while (n>0) {
+//
+//   }
+// }
